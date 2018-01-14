2a96791c8607bfb1e5bde54499c7870aa92817b4
shilpavijay <shilpa.amberker@gmail.com>
2018-01-15 00:36:26 +0530

    gridlines added

--- Scatterplot/scatterplot.js
+++ Scatterplot/scatterplot.js
@@ -34,7 +34,66 @@ var responseScale = d3.scale.linear()
 					}))
 					.range([2,15])		
 
+var tickValue = [18,25,32,39,45,52,60,68,74];
+
+var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom")	
+			.tickValues(tickValue);
+
+var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left")	
+			.ticks(5)	
+			.tickSize(20)
+			.tickFormat(function(d){
+				return d.toFixed(1)
+			});
+
+var xGridline = d3.svg.axis()
+				.scale(x)
+				.orient("bottom")
+				.tickSize(height,height)
+				.tickFormat("")
+				.tickValues(tickValue)
+
+var yGridline = d3.svg.axis()
+				.scale(y)
+				.orient("left")
+				.tickSize(-width,0,0)	
+				.tickFormat("")											
+
+function drawAxis(params){
+	if(params.initialize){
+
+		this.append("g")
+			.classed("x gridlines", true)
+			.attr("transform", "translate(0,0)")
+			.call(params.axis.gridlines.x)
+
+		this.append("g")
+			.classed("y gridlines", true)
+			.attr("transform", "translate(0,0)")
+			.call(params.axis.gridlines.y)
+
+		this.append("g")
+			.classed("x axis",true)
+			.attr("transform","translate(0," + height + ")")
+			.call(params.axis.x)
+
+		this.append("g")
+			.classed("y axis", true)
+			.attr("transform", "translate(0,0)")
+			.call(params.axis.y)
+	}
+	
+}
+
+
 function plot(params){
+
+	drawAxis.call(this,params);
+
 	var self = this;
 	var donuts = d3.keys(params.data[0]).filter(function(k){
 		return k !== 'age' && k !== 'responses'
@@ -90,5 +149,14 @@ function plot(params){
 }	
 
 plot.call(chart,{
-	data: data
+	data: data,
+	axis: {
+		x: xAxis,
+		y: yAxis,
+		gridlines: {
+			x: xGridline,
+			y: yGridline
+		}
+	},
+	initialize: true
 })	
